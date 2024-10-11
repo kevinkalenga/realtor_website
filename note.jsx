1f1332b882b5f6b423d3f40fb2a41a5b0003ad86
kevin kalenga <kevinkalenga10@gmail.com>
2024-10-11 13:16:53 +0200

    Add functionality including onchange and onsubmit function to the create listing page

---
+++ note.jsx
@@ -0,0 +1,20 @@
+/*
+https://console.cloud.google.com/google/maps-apis/credentials?utm_source=Docs_CreateAPIKey&utm_content=Docs_geocoding-backend&hl=fr&_gl=1*16ng54v*_ga*MjkyNTU0NTQ5Ni4xNzE4MDY2NjI3*_ga_NRWSTWS78N*MTcyODYzMTU1OC4yLjEuMTcyODYzNTEwOC4wLjAuMA..&project=realtor-website-react-9eb4d
+
+
+
+*/
+
+/*
+
+Accédez à la page Google Maps Platform > Identifiants.
+
+Accéder à la page "Identifiants"
+
+Sur la page Identifiants, cliquez sur Créer des identifiants > Clé API.
+La boîte de dialogue Clé API créée affiche la clé API que vous venez de créer.
+Cliquez sur Fermer.
+La nouvelle clé API figure sur la page Identifiants sous Clés API.
+(N'oubliez pas de restreindre la clé API avant de l'utiliser en production.)
+
+*/ 
