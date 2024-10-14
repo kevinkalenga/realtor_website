848b40cc559a8ce28018a8aef1c1fbe802a5115b
kevin kalenga <kevinkalenga10@gmail.com>
2024-10-15 01:15:21 +0200

    Add my listings section to the profile page and create the listingItem component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,20 @@
 import { getAuth, updateProfile } from 'firebase/auth';
-import { doc, updateDoc } from 'firebase/firestore';
-import React, { useState } from 'react';
+import { doc, updateDoc,collection, where, query, orderBy,getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import {db} from "../firebase";
 import { toast } from 'react-toastify';
 import { FcHome } from "react-icons/fc";
 import { Link } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 export default function Profile() {
+
   const auth = getAuth();
   const navigate = useNavigate();
   const [changeDetail, setChangeDetail] = useState(false);
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
   const [formDate, setFormData] = useState({
     name:auth.currentUser.displayName,
     email:auth.currentUser.email
@@ -47,6 +51,25 @@ export default function Profile() {
       toast.error("Could not update the profile details")
      }
   }
+  useEffect(()=>{
+     async function fetchUserListings() {
+      
+         const listingRef = collection(db, "listings");
+         const q = query(listingRef, where("userRef", "==", auth.currentUser.uid), orderBy("timestamp", "desc"))
+         const querySnap = await getDocs(q)
+           console.log(querySnap)
+         let listings = [];
+         querySnap.forEach((doc)=>{
+             return listings.push({
+               id: doc.id,
+               data: doc.data(),
+             })
+         })
+         setListings(listings);
+         setLoading(false)
+     }
+     fetchUserListings();
+  }, [auth.currentUser.uid])
   return (
     <>
       <section className='max-w-6xl flex mx-auto justify-center items-center flex-col'>
@@ -94,6 +117,20 @@ export default function Profile() {
           </button>
         </div>
       </section>
+      <div className='max-w-6xl px-3 mt-6 mx-auto'>
+        {
+          !loading && listings.length > 0 &&(
+            <>
+              <h2 className='text-2xl text-center font-semibold'>My listings</h2>
+              <ul>
+                {listings.map((listing) => (
+                    <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
+                ))}
+              </ul>
+            </>
+          )
+        }
+      </div>
     </>
   )
 }
